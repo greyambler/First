c83b5f32e5b0bfffe55e174e424a94fb72c96f29
greyambler <greyambler@rambler.ru>
2019-03-18 18:03:34 +0300

    Revert "N20"
    This reverts commit 43994e56440b5c441646bb923a29e262ea67afd9.

--- src/core/N_List.jsx
+++ src/core/N_List.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 
 class N_List extends React.Component {
+   constructor(props) {
+      super(props);
+
+      this.state = { List: this.props.obList };
+   }
+   componentDidUpdate(prevProps) {
+      this.setState({ 'List': prevProps.ListVal })
+   }
+   /*
+   this.display = this.display.bind(this);
+ 
+ display() {
+
+    return "";
+ } */
    render() {
       const List = this.props.ListVal;
       if (List != null) {
@@ -8,6 +23,17 @@ class N_List extends React.Component {
             <ol className="NumList">
                {
                   List.map(el => (
+                     <li key={el.id}>{el.nm}</li>
+                  ))
+               }
+            </ol>
+         );
+      }
+      else if (this.state.List != null) {
+         return (
+            <ol className="NumList">
+               {
+                  this.state.List.map(el => (
                      <li key={el.id}>{el.nm}</li>
                   ))
                }
